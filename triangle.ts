0e65f4df7b766b7930d391d3eaac21b10ef1c4e9
Jim <94252877+Jimhoff06@users.noreply.github.com>
2023-06-29 17:24:33 +0000

    completed exercise

--- triangle.ts
+++ triangle.ts
@@ -1,1 +1,35 @@
-export class Triangle {}
+export class Triangle {
+  private sortedSides: Array<number>;
+  isEquilateral: boolean;
+  isIsosceles: boolean;
+  isScalene: boolean;
+  private testTriangleInequality(): boolean {
+    if (this.sortedSides[2] <= 0) {
+      return false;
+    }
+    return this.sortedSides[0] < this.sortedSides[1] + this.sortedSides[2];
+  }
+  private testIsEquilateral(): boolean {
+    return this.testTriangleInequality() && this.duplicateSides().length > 1;
+  }
+  private testIsIsosceles(): boolean {
+    return this.testTriangleInequality() && this.duplicateSides().length >= 1;
+  }
+  private testIsScalene(): boolean {
+    return this.testTriangleInequality() && this.duplicateSides().length === 0;
+  }
+  private duplicateSides() {
+    return this.sortedSides.filter(
+      (side, index) => this.sortedSides.indexOf(side) !== index
+    );
+  }
+
+  constructor(a: number, b: number, c: number) {
+    this.sortedSides = [a, b, c].sort((a, b) => {
+      return b - a;
+    });
+    this.isEquilateral = this.testIsEquilateral();
+    this.isIsosceles = this.testIsIsosceles();
+    this.isScalene = this.testIsScalene();
+  }
+}
